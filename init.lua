53d78ce291049fad6114acaf7d0771b99a64b3f5
Technik97 <aniket.97.mane@gmail.com>
2024-09-19 21:56:39 +0530

    Installed neo-tree

--- init.lua
+++ init.lua
@@ -20,4 +20,13 @@ vim.opt.rtp:prepend(lazypath)
 --    :Lazy update
 --
 -- NOTE: Here is where you install your plugins.
-require('lazy').setup({})
+require('lazy').setup({
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    }
+})
