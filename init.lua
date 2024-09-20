007a31f16233effd066efb266c928cf6d556981f
Technik97 <aniket.97.mane@gmail.com>
2024-09-20 17:43:00 +0530

    Added bufferline plugin for tabs

--- init.lua
+++ init.lua
@@ -25,5 +25,6 @@ vim.opt.rtp:prepend(lazypath)
 -- NOTE: Here is where you install your plugins.
 require('lazy').setup({
     require 'plugins.neotree',
-    require 'plugins.colortheme'
+    require 'plugins.colortheme',
+    require 'plugins.bufferline',
 })
