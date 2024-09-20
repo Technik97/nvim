4248462516c7063e73d07c3e930a09ce14af31a9
Technik97 <aniket.97.mane@gmail.com>
2024-09-20 14:45:22 +0530

    Moved colortheme config to colortheme.lua

--- init.lua
+++ init.lua
@@ -34,20 +34,5 @@ require('lazy').setup({
           -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
         }
     },
-    {
-	'shaunsingh/nord.nvim',
-	lazy = false,
-	priority = 1000,
-	config = function()
-            vim.g.nord_contrast = true
-            vim.g.nord_borders = false
-            vim.g.nord_disable_background = false
-            vim.g.nord_italic = false
-            vim.g.nord_uniform_diff_background = true
-            vim.g.nord_bold = false
-
-
-            require('nord').set()
-	end 
-    }
+    require 'plugins.colortheme'
 })
